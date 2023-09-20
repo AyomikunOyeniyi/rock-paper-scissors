442224db6581fc14c6793b7b39be2ee03cf154a0
Ayomikun Oyeniyi <ayomikunoyeniyi07@gmail.com>
2023-09-20 14:57:51 -0400

    Write pseudocode for game

---
+++ script.js
@@ -0,0 +1,7 @@
+//When user inputs choice {rock,paper, or scissors}
+//Computer generates random output {rock, paper, or scissors}
+
+//winner can be decided using if statements
+//Computer displays who won
+//game plays for five rounds
+
