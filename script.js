a28041d6d82ce2a3ae64cb4046065dcf3ce6c527
Ayomikun Oyeniyi <ayomikunoyeniyi07@gmail.com>
2023-09-22 16:37:43 -0400

    Include variables to keep track of both scores

--- script.js
+++ script.js
@@ -5,6 +5,9 @@
 //Computer displays who won
 //game plays for five rounds
 
+let playerScore = 0;
+let computerScore = 0;
+
 let num = Math.random();
 
 let computerChoice = num.toFixed(2);
@@ -35,23 +38,34 @@ function playRound(playerSelection, computerSelection) {
     }
 
     if ( playerSelection == 'rock' && computerSelection == 'paper') {
-        return string = "You lose!Paper wins";
+        computerScore += 1;
+        return "You lose!Paper wins";
     } else if (playerSelection == 'rock' && computerSelection == 'scissors') {
+        playerScore += 1;
         return "You win! Scissors loses";
     } 
 
     if (playerSelection == 'paper' && computerSelection == 'rock') {
-        return string = "You win!Rock loses";
+        playerScore += 1;
+        return "You win!Rock loses";
     } else if (playerSelection == 'paper' && computerSelection == 'scissors') {
+        computerScore += 1;
         return "You lose!Scissors wins"
     }
 
     if (playerSelection == 'scissors' && computerSelection == 'rock') {
-        return string = "You lose! Rock wins";
+        computerScore += 1;
+        return "You lose! Rock wins";
     } else if (playerSelection == 'scissors' && computerSelection == 'paper') {
+        playerScore += 1;
         return "You win! Paper loses";
     }
+
 }
 
 console.log(playRound(playerSelection,computerSelection));
+console.log(playerScore, computerScore);
+
+
+
     
