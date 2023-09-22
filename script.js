421704c15beb1ed6027078ad06be311f2d61d1d6
Ayomikun Oyeniyi <ayomikunoyeniyi07@gmail.com>
2023-09-22 10:07:18 -0400

    Add code for player vs computer

--- script.js
+++ script.js
@@ -20,5 +20,38 @@ if (computerChoice <= 0.3) {
 function getComputerChoice() {
     return computerChoice;
 }
-console.log(getComputerChoice())
 
+
+
+let playerChoice = prompt('Insert choice here')
+
+let playerSelection = playerChoice.toLowerCase();
+
+const computerSelection = getComputerChoice().toLowerCase();
+
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === computerSelection) {
+        return "It's a tie!";
+    }
+
+    if ( playerSelection == 'rock' && computerSelection == 'paper') {
+        return string = "You lose!Paper wins";
+    } else if (playerSelection == 'rock' && computerSelection == 'scissors') {
+        return "You win! Scissors loses";
+    } 
+
+    if (playerSelection == 'paper' && computerSelection == 'rock') {
+        return string = "You win!Rock loses";
+    } else if (playerSelection == 'paper' && computerSelection == 'scissors') {
+        return "You lose!Scissors wins"
+    }
+
+    if (playerSelection == 'scissors' && computerSelection == 'rock') {
+        return string = "You lose! Rock wins";
+    } else if (playerSelection == 'scissors' && computerSelection == 'paper') {
+        return "You win! Paper loses";
+    }
+}
+
+console.log(playRound(playerSelection,computerSelection));
+    
